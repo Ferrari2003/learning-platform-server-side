a6bd59f958e613e6ab02b823693d6e997c22c094
Saiful Islam <https://github.com/Ferrari2003>
2022-10-28 03:53:02 +0600

    aded commit

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
   const express = require('express')
   const app = express();
+  var cors = require('cors')
   const prot = process.env.PORT || 5000;
 
+  app.use(cors())
 
-   const categories = require('./data/')
+
+   const categories = require('./data/course.json')
 
    app.get('/', (req, res) => {
        res.send('Details API Running')
    });
    
+   app.get('/details-categories', (req, res) => {
+    res.send(categories)
+   })
 
    app.listen(prot, () => {
        console.log('Programming data server running on port', prot);
